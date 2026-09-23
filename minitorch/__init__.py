b5917e553204ec268fc335090305f82e06c4a8c3
shuvokhan3 <shuvoj360@gmail.com>
2026-09-23 17:41:25 +0600

    # test for ,forward computation + computation history

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,7 +1,6 @@
 from .autodiff import central_difference
 from .scalar import Scalar
-
-
+from minitorch.scalar import Scalar
 
 
 
@@ -15,3 +14,20 @@ print(f"a.derivative = {a.derivative}")
 a.accumulate_derivative(1.0)
 a.accumulate_derivative(2.0)
 print(f"After accumulating: a.derivative = {a.derivative}")
+
+
+
+
+
+# test for ,forward computation + computation history
+a = Scalar(2.0)
+a.requires_grad_(True)
+
+b = Scalar(3.0)
+b.requires_grad_(True)
+
+c = a * b
+
+print(f"c.data = {c.data}")
+print(f"c.is_leaf() = {c.is_leaf()}")
+print(f"c.history.last_fn = {c.history.last_fn}")
