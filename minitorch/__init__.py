326b76aa4b60ebc1cf66c6cbf8cd943bfbf9ab66
shuvokhan3 <shuvoj360@gmail.com>
2026-09-25 17:47:29 +0600

    make chages

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,33 +1,78 @@
-from .autodiff import central_difference
+from .autodiff import central_difference ,topological_sort, backpropagate
 from .scalar import Scalar
 from minitorch.scalar import Scalar
 
 
 
-a = Scalar(2.0, name="a")
-b = Scalar(3.0, name="b")
+# a = Scalar(2.0, name="a")
+# b = Scalar(3.0, name="b")
 
-print(f"a.data = {a.data}")
-print(f"a.is_leaf() = {a.is_leaf()}")
-print(f"a.derivative = {a.derivative}")
+# print(f"a.data = {a.data}")
+# print(f"a.is_leaf() = {a.is_leaf()}")
+# print(f"a.derivative = {a.derivative}")
 
-a.accumulate_derivative(1.0)
-a.accumulate_derivative(2.0)
-print(f"After accumulating: a.derivative = {a.derivative}")
-
-
+# a.accumulate_derivative(1.0)
+# a.accumulate_derivative(2.0)
+# print(f"After accumulating: a.derivative = {a.derivative}")
 
 
 
-# test for ,forward computation + computation history
+
+
+# # test for ,forward computation + computation history
+# a = Scalar(2.0)
+# a.requires_grad_(True)
+
+# b = Scalar(3.0)
+# b.requires_grad_(True)
+
+# c = a * b
+
+# print(f"c.data = {c.data}")
+# print(f"c.is_leaf() = {c.is_leaf()}")
+# print(f"c.history.last_fn = {c.history.last_fn}")
+
+
+
+# from minitorch.scalar import Scalar
+
+# a = Scalar(2.0)
+# a.requires_grad_(True)
+# b = Scalar(3.0)
+# b.requires_grad_(True)
+
+# c = a * b     # c = 6
+# d = c + a     # d = 6 + 2 = 8
+# d.backward()
+
+# print(f"a.derivative = {a.derivative}")  # d/da
+# print(f"b.derivative = {b.derivative}")  # d/db
+
+
+from minitorch.scalar import Scalar
+from minitorch.autodiff import backpropagate
+
 a = Scalar(2.0)
 a.requires_grad_(True)
 
 b = Scalar(3.0)
 b.requires_grad_(True)
 
-c = a * b
+c = Scalar(44.0)
+c.requires_grad_(True)
 
-print(f"c.data = {c.data}")
-print(f"c.is_leaf() = {c.is_leaf()}")
-print(f"c.history.last_fn = {c.history.last_fn}")
+f= (a * b) / c;
+
+print("Before backward:")
+print(f"a.derivative = {a.derivative}")
+print(f"b.derivative = {b.derivative}")
+print(f"c.derivative = {c.derivative}")
+print(f"c.derivative = {f.derivative}")
+
+backpropagate(f)
+
+print("\nAfter backward:")
+print(f"a.derivative = {a.derivative}")
+print(f"b.derivative = {b.derivative}")
+print(f"c.derivative = {c.derivative}")
+print(f"c.derivative = {f.derivative}")
